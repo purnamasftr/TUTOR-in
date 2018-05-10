a4af22b4b000d3c1f76ec0f4dfb13e707a61c1a6
wdita <waw.dita@gmail.com>
2018-05-10 11:08:10 +0700

    Add files via upload
    ada buat edit user

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,23 @@
 <?php
 
-Route::get('/', function(){
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/blog', 'BlogController@index');
-Route::get('/blog/{id}', 'BlogController@show');
+Route::resource('users', 'UserController');
+Route::get('users/{user}',  ['as' => 'users.edit', 'uses' => 'UserController@edit']);
+Route::patch('users/{user}/update',  ['as' => 'users.update', 'uses' => 'UserController@update']);
+Auth::routes('');
 
-
-Route::get('/admin-home', 'AdminController@AdminHome');
-Route::get('/admin-users', 'AdminController@AdminUser');
-
-<!-- //WebController -->
-Route::get('/web-home', 'WebController@WebHome');
-Route::get('/web-login', array('uses' => 'WebController@WebLogin', 'as' => 'pages.login'));
-
-<!-- //UserController -->
-Route::get('/user-home', 'UserController@User');
-
-Route::get('/blog', 'BlogController@index');
-
+Route::get('/home', 'HomeController@index')->name('home');
