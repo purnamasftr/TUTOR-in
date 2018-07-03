97cbcd5c4354554d6950217daa8826e7c178b426
purnama syafitri <purnamasftr@gmail.com>
2018-07-03 15:30:11 +0700

    Update ubah-sandi.blade.php

--- resources/views/users/ubah-sandi.blade.php
+++ resources/views/users/ubah-sandi.blade.php
@@ -21,7 +21,7 @@
                                       <button type="button" onclick="window.location='{{ route('users.ubah-sandi') }}'" class="btn btn-success">Ubah Password</button>
                                       @if( Auth::user()->type=='2' )
                                       <button type="button" onclick="window.location='{{ route('kelas.edit-kelas') }}'" class="btn btn-success">Kelas</button>
-                                      <button type="button" onclick="window.location='{{ route('users.edit-pengalaman') }}'" class="btn btn-success">Pengalaman</button>
+
                                       @endif
                                   </div>
                                 </div>
@@ -33,6 +33,16 @@
                            <div class="card">
                                <div class="card-body">
                                    <div class="basic-form">
+                                     @if (session('error'))
+                                         <div class="alert alert-danger">
+                                             {{ session('error') }}
+                                         </div>
+                                     @endif
+                                         @if (session('success'))
+                                             <div class="alert alert-success">
+                                                 {{ session('success') }}
+                                             </div>
+                                         @endif
                                        <form class="form-horizontal" method="POST" action="{{ route('changePassword') }}">
                                            {{ csrf_field() }}
                                          <div class="form-group{{ $errors->has('current-password') ? ' has-error' : '' }}">
